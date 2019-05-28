b4c2ba677176fc48119f19c1754b8f94134d610a
linzoe28 <zoe841228@gmail.com>
2019-05-28 12:01:17 +0800

    initial commit

---
+++ src/sample.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+class Application extends React.Component {
+    render() {
+        return (
+            <h5>test!</h5>
+        );
+    }
+}
+
+module.exports=Application;
